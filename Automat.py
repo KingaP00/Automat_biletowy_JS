194304c62c44a3c51602b828e5d243d334074815
KingaP00 <79651902+KingaP00@users.noreply.github.com>
2021-06-04 10:48:03 +0200

    usunięte niepotrzebne funkcje

--- Automat.py
+++ Automat.py
@@ -1,27 +1,18 @@
 from Monety import *
-from decimal import *
 
-class Automat(Monety):
+class Automat():
     suma = 0
 
     def init(self):
         super().init()
 
-    def sprzedajBilet(self, cena):
+    def odejmijBilet(self, cena):
         self.suma -= cena
-        print('Chcesz sprzedać bilet o wartości : ' + str(cena/100))
+        print('Odjęto bilet o wartości : ' + str(cena/100) + 'zł')
 
-    def kupBilet(self, cena):
+    def dodajBilet(self, cena):
         self.suma += cena
-        print('Chcesz kupić bilet o wartości : ' + str(cena/100))
-
-    def sumuj(self):
-        return self.suma
-
-    def zakoncz(self):
-        self.suma = 0
-
-        print('Dziękujemy za zakup biletu')
+        print('Dodano bilet o wartości : ' + str(cena/100) + 'zł')
 
     def pobierz_suma(self):
         return str(self.suma/100)
